32ddd9119945fbac198e5820c6ce57edc81c63aa
Kanishka Jayashanka <117253020+KJayashanka@users.noreply.github.com>
2026-09-18 09:15:58 +0530

    Set basename for BrowserRouter in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,7 @@ import { ThemeProvider } from './context/ThemeContext.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/Portfolio">
         <App />
       </BrowserRouter>
     </ThemeProvider>
